678c4c39047cc2ccf3832db298e6f44b5a38f96d
Ariel Fonticiella <arielfonticiella@Ariels-MacBook-Pro.local>
2017-11-21 15:35:04 -0500

    Initial Commit. Project2. Added hero image and set up routes for signup and login.

---
+++ models/user-model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// new Schema({ schema }, { settings }) -- This is the basic structure:
+const userSchema = new Schema(
+  // 1st argument -> SCHEMA STRUCTURE
+  {
+    userName: {
+      type: String,
+      required: [true, "Give us your User Name."]
+    },
+
+    encryptedPassword: {
+      type: String,
+    },
+
+    // normal login users:
+    email: {
+      type: String,
+      match: [/.+@.+/, "Emails need an @ sign"]
+    }
+  },
+    // 2nd argument -> SETTINGS object
+  {
+    // automatically add "createdAt" and "updatedAt" Date fields
+    timestamps: true
+  }
+);
+
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
---
+++ routes/user-router.js
@@ -0,0 +1,185 @@
+const express   = require("express");
+const bcrypt    = require("bcrypt");
+const passport  = require("passport");
+
+const UserModel = require("../models/user-model");
+
+const router = express.Router();
+
+// STEP #1: Show the sign up form
+router.get("/signup", (req, res, next) => {
+    // redirect to home if you are already logged in
+    if (req.user) {
+        res.redirect("/");
+
+        // early return to stop the function since there's an error
+        // prevent the rest of the code from running.
+        return;
+      }
+      res.render("user-views/signup-page");
+});
+
+//STEP #2: process the sign up form
+router.post("/process-signup", (req, res, next) => {
+    if (req.body.signupPassword === ""      ||
+        req.body.signupPassword.length < 11 ||
+        req.body.signupPassword.match(/[^a-z0-9]/i) === null
+      ){                        //          |
+                                // no special characters
+          // display the form again if it is
+          res.locals.errorMessage = "Password is invalid";
+          res.render("user-views/signup-page");
+          // early return to stop the function since there's an error
+          // prevent the rest of the code from running.
+          return;
+      }
+      // query the database to see if the email is taken
+      UserModel.findOne({ email: req.body.signupEmail })
+        .then((userFromDb) => {
+          // userFromDb will be null if the email IS NOT taken
+
+          // display the form again if the email is taken
+          if (userFromDb !== null) {
+            res.locals.errorMessage = "Email is Taken";
+            res.render("user-views/signup-page");
+
+            // early return to stop the function since there's an error
+            // prevent the rest of the code from running.
+            return;
+          }
+
+          // generate a new salt for this user's password
+          const salt = bcrypt.genSaltSync(10);
+
+          // encrypted the password submitted from the form
+          //                                             |
+          const scrambledPassword = bcrypt.hashSync(req.body.signupPassword, salt);
+
+          const theUser = new UserModel({
+            userName:          req.body.signupUserName,
+            encryptedPassword: scrambledPassword,
+            email:             req.body.signupEmail
+          });
+
+          // return the promise of the next database query
+          // to continue the sequence
+          return theUser.save();
+        })
+            .then(() => {
+              // redirect to the home page on a successful sign up
+              res.redirect("/");
+            })
+            .catch((err) => {
+                next(err);
+            });
+}); // POST /process-signup
+
+//STEP #1: show the login form
+router.get("/login", (req, res, next) => {
+  // redirect to home if you are already logged in
+  if (req.user) {
+      res.redirect("/");
+
+      // early return to stop the function since there's an error
+      // prevent the rest of the code from running.
+      return;
+    }
+    res.render("user-views/login-page");
+});
+
+//STEP #2: process the log in form
+router.post("/process-login", (req, res, next) => {
+    // find a user documetn in the database with that email
+    UserModel.findOne({ email: req.body.loginEmail })
+      .then((userFromDb) => {
+          // if we didn't find a user
+          if (userFromDb === null) {
+              //disply the form again because the email is wrong
+              res.locals.errorMessage = "Incorrect Email.";
+              res.render("user-views/login-page");
+
+              // early return to stop the function since there's an error
+              // prevent the rest of the code from running.
+              return;
+          }
+          // if email is correct now we check the password
+          const isPasswordGood =
+            bcrypt.compareSync(req.body.loginPassword, userFromDb.encryptedPassword);
+
+          if (isPasswordGood === false) {
+              res.locals.errorMessage = "Incorrect Password";
+              res.render("user-views/login-page");
+
+              // early return to stop the function since there's an error
+              // prevent the rest of the code from running.
+              return;
+          }
+          //CREDENTIALS ARE GOOD! We need to log the user in.
+
+          // Passport defines the "req.login()"
+          // for us to specify when to login a user into the sesison.
+          req.login(userFromDb, (err) => {
+            // check to see if the log in worked
+            if (err) {
+                // if it didn't work show the error page
+                next(err);
+            }
+            else {
+                // if it worked redirect to the home page
+                res.redirect("/");
+            }
+          }); // req.login()
+      }) // then()
+      .catch((err) => {
+            next(err);
+      });
+});
+
+router.get("/logout", (req, res, next) => {
+    // Passport defines the "req.logout()" method
+    // for us to specify when to log out a user (clear them from the session)
+    req.logout();
+
+    res.redirect("/");
+});
+
+
+// // Facebook login routes
+// //---------------------------------------------------------------------
+// // Link to "/facebook/login" to initiate the login process
+// router.get("/facebook/login",
+//   passport.authenticate("facebook"));
+//                                         //              |
+//                                         // This name comes from the Strategy
+//
+// // Facebook will redirect here after login is successful
+// router.get("/facebook/success",     // no normal callback here
+//   passport.authenticate("facebook", {
+//       successRedirect: "/",
+//       failureRedirect: "/login"
+//   })
+// );
+//
+// // Google login routes
+// //---------------------------------------------------------------------
+//
+// // Link to "/google/login" to initiate the login process
+// router.get("/google/login",
+//   passport.authenticate("google", {
+//     scope: [
+//       "https://www.googleapis.com/auth/plus.login",
+//       "https://www.googleapis.com/auth/plus.profile.emails.read"
+//     ]
+//   })
+// );
+//
+// // Google will redirect here after login is successful
+// router.get("/google/success",     // no normal callback here
+//   passport.authenticate("google", {
+//       successRedirect: "/",
+//       failureRedirect: "/login"
+//   })
+// );
+
+
+module.exports = router;
